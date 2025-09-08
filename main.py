c11758f290b11ecaa09ae1525e4f0aa1dd3bc76b
friendiizcr <friendiizcr@gmail.com>
2025-09-08 14:12:11 +0700

    Add /decode-qr endpoint to main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+from pyzbar.pyzbar import decode
+import io
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "FastAPI is working!"}
+
+@app.post("/decode-qr")
+async def decode_qr(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes))
+    qr_data = decode(image)
+
+    if not qr_data:
+        return {"result": "No QR code found"}
+
+    return {"result": qr_data[0].data.decode("utf-8")}
